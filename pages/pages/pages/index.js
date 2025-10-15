a976e2b7ecb42a2f761a9701f27e499b003d40a6
edgarbrice <alimentosmanada22@gmail.com>
2025-10-14 22:36:34 -0300

    Create index.js

---
+++ pages/pages/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", textAlign: "center", padding: 50 }}>
+      <h1>Bienvenido a Alimentos MANADA 🐾</h1>
+      <p>Nutrición de calidad para tu regalón.</p>
+    </div>
+  );
+}
