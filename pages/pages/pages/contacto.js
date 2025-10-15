fde2b933864aae1bb7548dd90b10c9296de15d1e
edgarbrice <alimentosmanada22@gmail.com>
2025-10-15 01:08:35 -0300

    Create contacto.js

---
+++ pages/pages/pages/contacto.js
@@ -0,0 +1,10 @@
+export default function Contacto() {
+  return (
+    <main style={{ padding: '32px' }}>
+      <h1>Contacto</h1>
+      <p>WhatsApp: +56 9 XXXX XXXX</p>
+      <p>Email: contacto@manada.cl</p>
+      <a href="/">Volver al inicio</a>
+    </main>
+  );
+}
